280917529ba0f384d72dfed8a77fdd06bb0fe447
hoondeveloper <hoonskyn@cau.ac.kr>
2021-07-16 00:25:18 +0900

    feat(frame): add grayscale color constants

---
+++ packages/frame/src/shared/colors.ts
@@ -0,0 +1,14 @@
+/**
+ * Grayscale Color Constants
+ */
+export const GRAY_11 = '#121619';
+export const GRAY_10 = '#21272A';
+export const GRAY_9 = '#383E43';
+export const GRAY_8 = '#5C6267';
+export const GRAY_7 = '#757C83';
+export const GRAY_6 = '#8C929B';
+export const GRAY_5 = '#A8AFB6';
+export const GRAY_4 = '#C8CED4';
+export const GRAY_3 = '#E1E5EA';
+export const GRAY_2 = '#F0F3F6';
+export const GRAY_1 = '#F6F7F9';
