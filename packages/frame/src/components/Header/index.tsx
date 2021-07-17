863ab6e51925de6f45fd6e783d028f72b5bbe8b1
hoondeveloper <hoonskyn@cau.ac.kr>
2021-07-18 01:20:19 +0900

    fix: add Header position fixed and some style

--- packages/frame/src/components/Header/index.tsx
+++ packages/frame/src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { IHeaderProps } from './PropsType';
 import { HeaderContainer, HeaderLeftArea, HeaderRightArea, HeaderTitle } from './Styled';
+import { css } from '@emotion/css';
 
 const Header: React.FC<IHeaderProps> = ({ leftArea, rightArea, children }) => {
 	return (
-		<HeaderContainer>
-			<HeaderLeftArea>{leftArea}</HeaderLeftArea>
-			<HeaderTitle>{children}</HeaderTitle>
-			<HeaderRightArea>{rightArea}</HeaderRightArea>
-		</HeaderContainer>
+		<header
+			className={css`
+				position: fixed;
+				top: 0;
+				left: 0;
+				right: 0;
+				background: #fff;
+			`}>
+			<HeaderContainer>
+				<HeaderLeftArea>{leftArea}</HeaderLeftArea>
+				<HeaderTitle>{children}</HeaderTitle>
+				<HeaderRightArea>{rightArea}</HeaderRightArea>
+			</HeaderContainer>
+		</header>
 	);
 };
 
